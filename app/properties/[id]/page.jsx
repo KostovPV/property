da657201faf03058cbe0214c71a0dd3e7dfa75c8
KostovPV <106186518+KostovPV@users.noreply.github.com>
2025-01-03 11:16:33 +0200

    PropertyDetails component created

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,8 +1,10 @@
+import PropertyDetails from "@/app/components/PropertyDetails";
 import PropertyHeaderImage from "@/app/components/PropertyHeaderImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
+
 
 const PropertyPage = async ({ params }) => {
     await connectDB();
@@ -23,7 +25,7 @@ const PropertyPage = async ({ params }) => {
             <section className="bg-blue-50">
                 <div className="container m-auto py-10 px-6">
                     <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-                    {/*  Property info   */}
+                    < PropertyDetails property={property} />
                     </div>
                 </div>
             </section>
