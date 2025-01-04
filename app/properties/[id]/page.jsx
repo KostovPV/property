564db4c492a9bde8d94c1543dcd9104863336c9f
KostovPV <106186518+KostovPV@users.noreply.github.com>
2025-01-04 12:25:26 +0200

    ProfileProperties edited to render the properties from the database

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -3,6 +3,7 @@ import PropertyHeaderImage from "@/app/components/PropertyHeaderImage";
 import PropertyImages from "@/app/components/PropertyImages";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convertToObject";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 
@@ -10,7 +11,14 @@ import { FaArrowLeft } from "react-icons/fa";
 const PropertyPage = async ({ params }) => {
     await connectDB();
     const id = await params.id;
-    const property = await Property.findById(id).lean();
+    const propertyDoc = await Property.findById(id).lean();
+    const property = convertToSerializableObject(propertyDoc);
+
+    if(!property) {
+        return (
+            <h1 className="text-center text-2xl font-bold mt-10">Property not found</h1>
+        )
+    }
     return (
         <>
             <PropertyHeaderImage image={property.images[0]} />
@@ -27,7 +35,7 @@ const PropertyPage = async ({ params }) => {
             <section className="bg-blue-50">
                 <div className="container m-auto py-10 px-6">
                     <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-                    < PropertyDetails property={property} />
+                        < PropertyDetails property={property} />
                     </div>
                 </div>
             </section>
