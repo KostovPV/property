afb33957cfc33d91be46a5a713e6f94766f49378
KostovPV <106186518+KostovPV@users.noreply.github.com>
2025-01-04 10:11:14 +0200

    Custom error page added and Image uploded to Cloudinary

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -4,12 +4,13 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import  cloudinary  from '@/config/cloudinary';
 
 async function addProperty(formData) {
     await connectDB();
 
     const sessionUser = await getSessionUser();
-    if(!sessionUser || !sessionUser.userId) {
+    if (!sessionUser || !sessionUser.userId) {
         throw new Error('User ID is required');
     }
 
@@ -20,7 +21,7 @@ async function addProperty(formData) {
     const images = formData
         .getAll('images')
         .filter((image) => image.name !== '')
-        .map((image) => image.name);
+
 
     const propertyData = {
         owner: userId,
@@ -47,8 +48,43 @@ async function addProperty(formData) {
             email: formData.get('seller_info.email'),
             phone: formData.get('seller_info.phone'),
         },
-        images
+    };
+
+    const imageUrls = [];
+
+    for (const imageFile of images) {
+        const imageBuffer = await imageFile.arrayBuffer();
+        const imageArray = Array.from(new Uint8Array(imageBuffer));
+        const imageData = Buffer.from(imageArray);
+
+        //Convert to base64
+        console.log('Converting image to base64');
+        const imageBase64 = imageData.toString('base64');
+        console.log('Uploading image to Cloudinary');
+
+        // const imageBase64 = imageData.toString('base64');
+
+        //Make request to cloudinary
+        // const result = await cloudinary.uploader.upload(`data:image/png;base64,${imageBase64}`, {
+        //     folder: 'propertypulse'
+        // });
+
+        // imageUrls.push(result.secure_url);
+        try {
+            const result = await cloudinary.uploader.upload(`data:image/png;base64,${imageBase64}`, {
+                folder: 'propertypulse'
+            });
+            imageUrls.push(result.secure_url);
+        } catch (error) {
+            console.error('Cloudinary upload failed:', error);
+            throw error; // Rethrow or handle as necessary
+        }
+
     }
+
+    propertyData.images = imageUrls;
+
+
     const newProperty = new Property(propertyData);
     await newProperty.save();
 
