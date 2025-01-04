bd5d0203a51999a63cd02d6898be57083cd413d2
KostovPV <106186518+KostovPV@users.noreply.github.com>
2025-01-04 11:01:56 +0200

    PropertyImages component added

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,6 @@
 import PropertyDetails from "@/app/components/PropertyDetails";
 import PropertyHeaderImage from "@/app/components/PropertyHeaderImage";
+import PropertyImages from "@/app/components/PropertyImages";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import Link from "next/link";
@@ -30,6 +31,7 @@ const PropertyPage = async ({ params }) => {
                     </div>
                 </div>
             </section>
+            <PropertyImages images={property.images} />
         </>
     );
 }
