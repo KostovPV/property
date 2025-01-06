1fa21d914b2dd70171f53b568c40306c2286ca4b
KostovPV <106186518+KostovPV@users.noreply.github.com>
2025-01-06 13:00:27 +0200

    Pagination logic implemented

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -3,20 +3,23 @@ import PropertyCard from '../components/PropertyCard';
 import Property from '@/models/Property';
 
 
-const PropertiesPage = async () => {
+const PropertiesPage = async ({ searchParams: { page = 1, pageSize = 2 } }) => {
     await connectDB();
-    const properties = await Property.find({}).lean();
+    const skip = (page-1) * pageSize;
+    const total = await Property.countDocuments({});
+    const properties = await Property.find({}).skip(skip).limit(pageSize);
+
 
     return (
         <section className='px-4 py-6'>
             <div className='container-xl lg:container m-auto px-4 py-6'>
                 {properties.length === 0 ? (<p>No properiess found</p>) : (
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                       {
-                        properties.map((property => (
-                            <PropertyCard key={property._id}  property={property} />
-                        )))
-                       } 
+                        {
+                            properties.map((property => (
+                                <PropertyCard key={property._id} property={property} />
+                            )))
+                        }
                     </div>
                 )}
             </div>
